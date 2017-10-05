4578ac9ff490796426885efde8922389a0b92f66
吴远洋 <wuyuanyang@wuyuanyangdeMacBook-Pro.local>
2017-10-05 19:39:27 +0800

    firstCommit

---
+++ app/src/main/java/app/wyyzzz/filepersistencetest/MainActivity.java
@@ -0,0 +1,53 @@
+package app.wyyzzz.filepersistencetest;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText edt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        edt = (EditText)findViewById(R.id.Edt);
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        String inputText = edt.getText().toString();
+        save(inputText);
+    }
+
+    public void save(String inputText){
+        FileOutputStream out = null;
+        BufferedWriter writer = null;
+        try{
+            out = openFileOutput("data", Context.MODE_PRIVATE);
+            writer = new BufferedWriter(new OutputStreamWriter(out));
+            writer.write(inputText);
+        }catch(IOException e){
+            e.printStackTrace();
+        }finally {
+            try{
+                if(writer != null){
+                    writer.close();
+                }
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+
+}
